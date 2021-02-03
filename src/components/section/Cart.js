a0896adc938b1a6ecd4ac09fac96a4324956d8e8
BhavikKheni <kheni.bhavik@gmail.com>
2021-02-03 15:17:38 +0530

    add cart

---
+++ src/components/section/Cart.js
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react'
+import { DataContext } from '../Context';
+import { Link } from 'react-router-dom';
+import Colors from './Colors'
+import './Cart.css'
+function Cart() {
+    const { cart, increase,reduction,removeProduct,total} = useContext(DataContext)
+    return (
+        <React.Fragment>
+            {
+                cart.length === 0 && 'No records found'
+            }
+             {
+                        cart.map(item =>(
+                            <div className="details cart" key={item._id}>
+                                <img src={item.src} alt=""/>
+                                <div className="box">
+                                    <div className="row">
+                                        <h2>{item.title}</h2>
+                                        <span>${item.price * item.count}</span>
+                                    </div>
+                                    <Colors colors={item.colors}/>
+                                    <p>{item.description}</p>
+                                    <p>{item.content}</p>
+                                    <div className="amount">
+                                        <button className="count" onClick={() => reduction(item._id)}> - </button>
+                                        <span>{item.count}</span>
+                                        <button className="count" onClick={() => increase(item._id)}> + </button>
+                                    </div>
+                                </div>
+                                <div className="delete" onClick={() => removeProduct(item._id)}>X</div>
+                            </div>
+                        ))
+                    }
+        </React.Fragment>
+    )
+}
+
+export default Cart;
